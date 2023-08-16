f7ae35be821fc227ecd0891666e8826c297b529f
Pia Martínez <19pia87@gmail.com>
2023-08-16 15:32:23 -0300

    funciones y manipulacion de img con bd

--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -2,8 +2,33 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel')
 
+var util = require('util');
+var cloudinary = require('cloudinary').v2;
+const uploader =util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
+
+/*listar novedades*/
 router.get('/', async function(req, res, next) {
     var novedades= await novedadesModel.getNovedades();
+
+    novedades = novedades.map(novedad => {
+        if (novedad.img_id){
+            const imagen = cloudinary.image(novedad.img_id, {
+                width:80,
+                height: 80,
+                crop: 'fill'
+            });
+            return{
+                ...novedad, //tit sub cuerpo
+                imagen //imagen
+            }
+        }else{
+            return{
+                ...novedad, //tit sub cuerpo
+                imagen: '' //nada
+            }
+        }
+    });
 
     res.render('admin/novedades',{
         layout:'admin/layout',
@@ -15,6 +40,11 @@ router.get('/', async function(req, res, next) {
   /*Para eliminar una novedad*/
   router.get('/eliminar/:id', async(req, res, next) =>{
     const id =req.params.id;
+
+    let novedad = await novedadesModel.getNovedadById(id);
+    if (novedad.img_id) {
+        await (destroy(novedad.img_id));
+    }
     await novedadesModel.deleteNovedadById(id);
     res.redirect('/admin/novedades')
   }) //cierra get de eliminar
@@ -23,14 +53,23 @@ router.get('/agregar', (req, res, next)=> {
     res.render('admin/agregar',{ //agregrar.hbs (dentro del admin)
         layout: 'admin/layout'
     }); //cierra render
-});
+}); //cierra Get
 
 router.post('/agregar', async (req, res, next) =>{
     try{
+        var img_id = '';
+        if (req.files && Object.keys(req.files).length > 0){
+            imagen = req.files.imagen;
+            img_id = (await uploader(imagen.tempFilePath)).public_id;
+        }
+
         //console.log(req.body)
         if(req.body.titulo !="" && req.body.subtitulo !="" &&
         req.body.cuerpo !=""){
-            await novedadesModel.insertNovedad(req.body);
+            await novedadesModel.insertNovedad({
+                ...req.body, //spread titulo, sub y cuerpo
+                img_id
+            });
             res.redirect('/admin/novedades')
         } else{
             res.render('admin/agregar',{
@@ -59,10 +98,27 @@ router.get('/modificar/:id', async (req, res, next)=>{
 /*metodo post- info que viene desde el formulario*/
 router.post('/modificar', async (req, res, next) => {
     try{
+        let img_id = req.body.img_original;
+        let borrar_img_vieja = false;
+        if (req.body.img_delete === "1"){
+            img_id=null;
+            borrar_img_vieja = true;
+        } else {
+            if(req.files && Object.keys(req.files).length > 0) {
+                imagen = req.files.imagen;
+                img_id = (await uploader(imagen.tempFilePath)).public_id;
+                borrar_img_vieja = true;
+            }
+        }
+        if (borrar_img_vieja && req.body.img_original){
+            await (destroy(req.body.img_original));
+        }
+
         var obj = {
             titulo:req.body.titulo,
             subtitulo:req.body.subtitulo,
-            cuerpo: req.body.cuerpo
+            cuerpo: req.body.cuerpo,
+            img_id
 }
 await novedadesModel.modificarNovedadById(obj, req.body.id);
 res.redirect('/admin/novedades');
--- routes/index.js
+++ routes/index.js
@@ -1,43 +1,64 @@
 var express = require('express');
 var router = express.Router();
-var nodemailer= require('nodemailer');
-var novedadesModel = require ('../models/novedadesModel');
+var nodemailer = require('nodemailer');
+var novedadesModel = require('../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
+router.get('/', async function (req, res, next) {
   var novedades = await novedadesModel.getNovedades();
+
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.url(novedad.img_id, {
+        width: 460,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: '/images/noimage.jpg'
+      }
+    }
+
+  });
   /* novedades = novedades.splice (0,5); */
-  res.render('index', { 
-    novedades 
+  res.render('index', {
+    novedades
   });
 });
+
 router.post('/', async (req, res, next) => {
   console.log(req.body)
   var nombre = req.body.nombre;
   var email = req.body.email;
-  var mensaje= req.body.mensaje;
+  var mensaje = req.body.mensaje;
 
   var obj = {
     to: '19pia87@gmail.com',
     subject: 'CONTACTO WEB',
-    html: nombre + "se contactó a través de la web y quiere más información a este correo: " + email + ".<br> Además hizo el siguiente comentario:" + mensaje  
+    html: nombre + "se contactó a través de la web y quiere más información a este correo: " + email + ".<br> Además hizo el siguiente comentario:" + mensaje
   }
 
   var transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
     auth: {
-      user:process.env.SMTP_USER,
-      pass:process.env.SMTP_PASS,
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
     }
-    
+
 
   });
 
-  var info= await transporter.sendMail(obj);
+  var info = await transporter.sendMail(obj);
 
   res.render('index', {
-    message:'Mensaje enviado correctamente'
+    message: 'Mensaje enviado correctamente'
   });
 
 });
